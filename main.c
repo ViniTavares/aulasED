1120e6a153ed0a7c458be58cc8afc16e4946d003
ViniTavares <58006533+ViniTavares@users.noreply.github.com>
2020-03-15 22:22:01 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,47 +2,101 @@
 #include <string.h>
 #include "PilhaEstatica.h"
 
-
-
-
-void iniciaPilha(pilhaEstatica *pilha) {
-pilha->topo=-1;
+void iniciaPilha(pilhaEstatica *pilha)
+{
+    pilha->topo=-1;
 }
 
-bool estaVazia(pilhaEstatica *pilha) {
- return (pilha->topo==-1);
+bool estaVazia(pilhaEstatica *pilha)
+{
+    return (pilha->topo==-1);
 }
 
-bool estaCheia(pilhaEstatica *pilha) {
-  return(pilha->topo==100);
-
-  }
-
-void empilha(Objeto item, pilhaEstatica *pilha) {
-pilha->topo++;
-pilha->array[pilha->topo]=item;
+bool estaCheia(pilhaEstatica *pilha)
+{
+    return(pilha->topo==100);
 }
 
-void desempilha(Objeto *item, pilhaEstatica *pilha) {
-*item = pilha->array[pilha->topo];
-pilha->topo--;
+void empilha(Objeto item, pilhaEstatica *pilha)
+{
+    pilha->topo++;
+    pilha->array[pilha->topo]=item;
 }
 
-int tamanhoPilha(pilhaEstatica *pilha) {
-return ((pilha->topo)-1);
+void desempilha(Objeto *item, pilhaEstatica *pilha)
+{
+    *item = pilha->array[pilha->topo];
+    pilha->topo--;
 }
 
-Objeto topo(pilhaEstatica *pilha) {
-  // adicione seu codigo aqui
+int tamanhoPilha(pilhaEstatica *pilha)
+{
+    return ((pilha->topo)+1);
 }
 
-void imprimePilha(pilhaEstatica *pilha) {
-  // adicione seu codigo aqui
+Objeto topo(pilhaEstatica *pilha)
+{
+    return pilha->array[pilha->topo];
 }
 
-int main(){
+void imprimePilha(pilhaEstatica *pilha)
+{
+    printf("\nPILHA:\n");
+    for(int i=0; i<=(pilha->topo); i++)
 
+    {
+        printf("%i\n",pilha->array[i]);
+    }
+}
 
+int main()
+{
+    pilhaEstatica pilha;
+    Objeto item;
+    int des;
 
-return 0;
+    iniciaPilha(&pilha);
+
+    printf("Digite os elementos da pilha:\n");
+
+    for(int i=0; i<MAXTAM; i++)
+    {
+        if(estaCheia(&pilha)== 0)
+        {
+            scanf("%i", &item);
+            empilha(item, &pilha);
+        }
+    }
+
+    imprimePilha(&pilha);
+
+    printf("Elemento do topo: %i\n", topo(&pilha));
+    printf("Tamanho da pilha: %i\n", tamanhoPilha(&pilha));
+    printf("\nDeseja desempilhar quantos elementos ?: ");
+
+    scanf("%i", &des);
+
+    if(des>0)
+    {
+        for(int i=0; i<des; i++)
+        {
+            desempilha(&item, &pilha);
+        }
+        printf("\nPilha apos o processo:\n");
+
+        imprimePilha(&pilha);
+
+        printf("Elemento do topo: %i\n", topo(&pilha));
+        printf("Tamanho da pilha: %i\n", tamanhoPilha(&pilha));
+    }
+    else
+    {
+        printf("\nPilha apos o processo:\n");
+
+        imprimePilha(&pilha);
+
+        printf("Elemento do topo: %i\n", topo(&pilha));
+        printf("Tamanho da pilha: %i\n", tamanhoPilha(&pilha));
+    }
+    return 0;
 }
